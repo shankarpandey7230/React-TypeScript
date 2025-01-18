36d780240e6c6e4788ec93f8be3f781cb3aed7df
shankarpandey7230 <shankarpandey7230@gmail.com>
2025-01-19 03:02:01 +1100

    return type

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import Component from "./learningFolder/01-return/index";
 function App() {
   return (
     <main>
-      <h2>React & Typescript</h2>
+      <Component />
     </main>
   );
 }
---
+++ src/learningFolder/01-return/index.tsx
@@ -0,0 +1,14 @@
+
+
+function Component(): JSX.Element | null | string{
+    return null;
+    return 'hello'
+    return (
+        <div>
+            <h2>React TypeScript</h2>
+            <h2>Return Type</h2>
+        </div>
+    )
+}
+
+export default Component;

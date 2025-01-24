f7789cb5f8f6715fd4af09e8420a58df562e2da1
shankarpandey7230 <shankarpandey7230@gmail.com>
2025-01-24 23:29:06 +1100

    challenges

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@
 // import Component from "./learningFolder/01-return/index";
 // import Component from "./learningFolder/props/index";
 
-import Component from "./learningFolder/04-events";
+import Component from "./learningFolder/05-challenge";
+
+// import Component from "./learningFolder/05-challenge";
+
+// import Component from "./learningFolder/04-events";
 
 // import Component from "./learningFolder/03-state";
 
@@ -15,8 +19,12 @@ function App() {
       </Component>
       <Component name="Astha" id={ 1234}/> */}
       {/* <Component/> */}
-      <Component/>
-
+      {/* <Component/> */}
+      <h2>React TypeScript</h2>
+      <Component type='advanced' name='susan' email='email@email.com' />
+            <Component type='basic' name='susan' email=''/>
+      {/* <Component type='basic' name='susan' /> */}
+      {/* <Component type="advanced" name="anna" email="email@email.com"/> */}
     </main>
   );
 }
